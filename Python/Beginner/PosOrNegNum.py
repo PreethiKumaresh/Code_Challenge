fa37a44ba74e6768c794468c4a78c34d19ad28bb
Preethi M K <38469016+PreethiKumaresh@users.noreply.github.com>
2019-06-06 18:38:02 +0530

    Update PosOrNegNum.py

--- Python/Beginner/PosOrNegNum.py
+++ Python/Beginner/PosOrNegNum.py
@@ -1,4 +1,7 @@
 n=int(input())
-if n < 0: print("Negative")
-elif n > 0: print("Postive")
-else: print("Zero")
+if n < 0: 
+  print("Negative")
+elif n > 0: 
+  print("Postive")
+else: 
+  print("Zero")
